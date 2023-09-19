6d89dabb73d20a6631a5bc40a5a28afa9bba0235
fabry-js <vivaldifabrizio10@gmail.com>
2023-09-19 22:37:46 +0200

    feat(renderer): :sparkles: add invoke method to preloader

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,15 +1,22 @@
+/* eslint-disable no-async-promise-executor */
 // Disable no-unused-vars, broken for spread args
 /* eslint no-unused-vars: off */
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
-export type Channels = 'ipc-example';
-
 const electronHandler = {
   ipcRenderer: {
-    sendMessage(channel: Channels, ...args: unknown[]) {
+    sendMessage(channel: string, ...args: unknown[]) {
       ipcRenderer.send(channel, ...args);
     },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
+    invoke(channel: string, args: unknown[]): Promise<any> {
+      return new Promise(async (resolve, reject) => {
+        ipcRenderer
+          .invoke(channel, args)
+          .then((response) => resolve(response))
+          .catch((error) => reject(error));
+      });
+    },
+    on(channel: string, func: (...args: unknown[]) => void) {
       const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
         func(...args);
       ipcRenderer.on(channel, subscription);
@@ -18,7 +25,7 @@ const electronHandler = {
         ipcRenderer.removeListener(channel, subscription);
       };
     },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
+    once(channel: string, func: (...args: unknown[]) => void) {
       ipcRenderer.once(channel, (_event, ...args) => func(...args));
     },
   },
