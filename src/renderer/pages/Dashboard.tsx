1d889e622caf8bce20a157a1bea8058b930cd045
fabry-js <vivaldifabrizio10@gmail.com>
2023-09-19 22:40:13 +0200

    feat(renderer): :sparkles: add Dashboard screen

---
+++ src/renderer/pages/Dashboard.tsx
@@ -0,0 +1,36 @@
+import ProjectCard from '../components/ProjectCard';
+import { INTERNAL_TOPA3_NEW_PROJECT_CARD } from '../utils/consts';
+import useRetrieveProjects from '../utils/useRetrieveProjects';
+
+function Dashboard() {
+  const { projects } = useRetrieveProjects();
+  return (
+    <div className="text-white flex flex-col p-10 justify-center gap-10">
+      <div>
+        <h1 className="text-4xl font-semibold">Benvenuto nella Dashboard.</h1>
+        <h2 className="text-2xl mt-2 font-semibold opacity-50">
+          Ecco i tuoi progetti
+        </h2>
+      </div>
+      <div className="flex flex-row items-center">
+        {projects && (
+          <div>
+            {projects.map((project) => (
+              <ProjectCard key={project.name} project={project} />
+            ))}
+          </div>
+        )}
+        <ProjectCard
+          key="__internal_newProjectCard"
+          project={{
+            content: '',
+            name: INTERNAL_TOPA3_NEW_PROJECT_CARD,
+            path: '',
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
