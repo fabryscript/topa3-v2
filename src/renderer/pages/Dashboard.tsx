cd0c95f9401e800eac6285c5210b928284f84ab0
fabry-js <vivaldifabrizio10@gmail.com>
2024-02-08 21:28:59 +0100

    feat: Add real-time terminal output state

--- src/renderer/pages/Dashboard.tsx
+++ src/renderer/pages/Dashboard.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
+import stripAnsi from 'strip-ansi';
 import FloatingTerminalButton from '../components/FloatingTerminalButton';
 import ProjectCard from '../components/ProjectCard';
-import { INTERNAL_TOPA3_NEW_PROJECT_CARD } from '../utils/consts';
 import useRetrieveProjects from '../utils/useRetrieveProjects';
+import UploadBox from '../components/UploadBox';
 
 function Dashboard() {
   const { projects } = useRetrieveProjects();
+  const [terminalOutput, setTerminalOutput] = useState<string[]>([]);
+
+  useEffect(() => {
+    window.electron.ipcRenderer.on('shell-result', (e) => {
+      const resultArray = e as Uint8Array;
+      const decoder = new TextDecoder('utf-8');
+      const result = decoder.decode(resultArray);
+
+      // Check if the result is already present in the terminalOutput array
+      if (!terminalOutput.includes(result)) {
+        // Append the result to the terminalOutput array
+        setTerminalOutput((prevOutput) => [...prevOutput, stripAnsi(result)]);
+      }
+    });
+  }, [terminalOutput]); // Add terminalOutput as a dependency to useEffect
+
   return (
     <div className="relative text-white flex flex-col p-10 justify-center gap-10">
       <div>
@@ -21,14 +39,7 @@ function Dashboard() {
             ))}
           </div>
         )}
-        <ProjectCard
-          key="__internal_newProjectCard"
-          project={{
-            content: '',
-            name: INTERNAL_TOPA3_NEW_PROJECT_CARD,
-            path: '',
-          }}
-        />
+        <UploadBox />
       </div>
       <FloatingTerminalButton />
     </div>
