a3e0e2c170647bcfe754f41bfefb4a01883b9dd6
fabry-js <vivaldifabrizio10@gmail.com>
2024-02-09 13:25:52 +0100

    feat: rework UploadBox

--- src/renderer/components/UploadBox.tsx
+++ src/renderer/components/UploadBox.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Upload } from 'lucide-react';
 import { getWorkspaceFolderName } from '../utils';
 
 function UploadBox() {
@@ -8,7 +9,7 @@ function UploadBox() {
   const [isFileInvalid, setIsFileInvalid] = useState(false);
   useEffect(() => setInitialAnimationTrigger(true), []);
   return (
-    <div className="uploadcontainer flex flex-col">
+    <div className="flex flex-col">
       {!filesInfo && (
         <div
           className={`${
@@ -16,8 +17,9 @@ function UploadBox() {
           } transition-opacity duration-1000 flex flex-col gap-5 px-10`}
         >
           <label htmlFor="file-upload">
-            <div className="flex flex-col justify-center items-center min-h-[200px] px-10 neomorphistic">
-              <p className="opacity-50">
+            <div className="flex flex-col gap-2 justify-center items-center min-h-[200px] px-10 border-4 rounded-2xl border-dashed opacity-60 hover:opacity-80 transition-opacity duration-200 ease-in-out">
+              <Upload className="w-11 h-auto" />
+              <p className="whitespace-nowrap">
                 Carica file <code>_topa3</code>
               </p>
             </div>
@@ -58,38 +60,40 @@ function UploadBox() {
           <p className="text-lg opacity-50">Stai per importare il progetto</p>
           <code>{getWorkspaceFolderName(filesInfo!.path)}</code>
           <p className="text-lg opacity-50">Vuoi confermare?</p>
-          <button
-            type="button"
-            className="confirmbutton bg-[#0C212A] border-2 border-[#44A1C9] min-w-[200px] min-h-[60px] rounded-2xl"
-            onClick={async () => {
-              window.electron.ipcRenderer
-                .invoke('new-project', [
-                  JSON.stringify({
-                    path: filesInfo.path,
-                    content: await filesInfo.text(),
-                    name: getWorkspaceFolderName(filesInfo.path),
-                  }),
-                ])
-                .then((res) => {
-                  if (res === 'ok') {
-                    return window.location.reload();
-                  }
-                  return null;
-                })
-                .catch((e) => {
-                  throw new Error(e.message);
-                });
-            }}
-          >
-            <p>Sì, confermo 🚀</p>
-          </button>
-          <button
-            type="button"
-            className="undobutton bg-[#2A0C0C] border-2 border-[#C23838] min-w-[200px] min-h-[60px] rounded-2xl"
-            onClick={() => setFilesInfo(undefined)}
-          >
-            <p>No, cancella ❌</p>
-          </button>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="inline-flex items-center py-2 px-4 bg-blue-200 rounded-2xl text-neutral-800"
+              onClick={async () => {
+                window.electron.ipcRenderer
+                  .invoke('new-project', [
+                    JSON.stringify({
+                      path: filesInfo.path,
+                      content: await filesInfo.text(),
+                      name: getWorkspaceFolderName(filesInfo.path),
+                    }),
+                  ])
+                  .then((res) => {
+                    if (res === 'ok') {
+                      return window.location.reload();
+                    }
+                    return null;
+                  })
+                  .catch((e) => {
+                    throw new Error(e.message);
+                  });
+              }}
+            >
+              <p>Sì, confermo</p>
+            </button>
+            <button
+              type="button"
+              className="inline-flex items-center py-2 px-4 border-blue-200 border rounded-2xl text-neutral-200"
+              onClick={() => setFilesInfo(undefined)}
+            >
+              <p>No, cancella</p>
+            </button>
+          </div>
         </div>
       )}
     </div>
